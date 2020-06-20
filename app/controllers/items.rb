d37511f0823e538b9c6af5056ae9a9f2132d4217
Ralph Brown <ralph.brown@sri.sandals.com>
2020-06-20 03:32:46 -0500

    Task2: Setup controller method '/items/create'

--- app/controllers/items.rb
+++ app/controllers/items.rb
@@ -6,4 +6,17 @@ Task1::App.controllers :items do
     get_items(item_limit)
   end
 
+  get :create do
+    redirect "/items/create/10"
+  end
+
+  get :create, :with => :amount do
+    amount = params[:amount].to_i
+    amount = 10 if amount == 0
+
+    table_name = create_temp_item_table
+    table_name
+      #insert_rows_to_temp_item_table(table_name, amount)
+
+  end
 end

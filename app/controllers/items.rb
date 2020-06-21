c6c72e2e3a15073919eb3133f53e5ec78959c838
Ralph Brown <ralph.brown@sri.sandals.com>
2020-06-21 00:47:22 -0500

    Accept an item limit as a parameter instead of fixed value.

--- app/controllers/items.rb
+++ app/controllers/items.rb
@@ -1,11 +1,4 @@
 Task1::App.controllers :items do
-
-  get :index do
-    #use item limit since result record count is large
-    item_limit = 2000
-    get_items(item_limit)
-  end
-
   get :create do
     redirect "/items/create/10"
   end
@@ -17,5 +10,17 @@ Task1::App.controllers :items do
     table_name = create_temp_item_table
     insert_rows_to_temp_item_table(table_name, amount)
     get_brand_code_count(table_name)
+  end
+
+  get :index do
+    redirect "/items/1000"
+  end
+
+  get :index, :with => :item_limit do
+    item_limit = params[:item_limit].to_i
+    item_limit = 1000 if item_limit == 0
+
+    #TODO: use paging instead of return huge chunk to client
+    get_items(item_limit)
   end
 end
